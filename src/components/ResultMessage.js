725c24dec9df26bb14c0ea1342d2ff9a99550267
Kate Yeeum <yeeumm@gmail.com>
2019-03-14 02:33:18 -0700

    ResultMessage component updated
    Display the number of result (search)

---
+++ src/components/ResultMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultMessage = ({ message }) => {
+    return (
+        <p className="list-result-number">
+            {message}
+        </p>
+    );
+}
+
+export default ResultMessage;
