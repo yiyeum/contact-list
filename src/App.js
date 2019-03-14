2cf3324879399281f140e7a070865b5db56eb1a0
Kate Yeeum <yeeumm@gmail.com>
2019-03-14 00:03:58 -0700

    Route the list view, detail view pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ListView from './containers/ListView';
+import DetailView from './containers/DetailView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <Switch>
+          <Route exact path="/" component={ListView} />
+          <Route exact path="/contacts/:id" component={DetailView} />
+        </Switch>
       </div>
     );
   }
